8520db1e27b500f243059ed5f6f4f4d6d3272f05
Steven Solomon <solomon.steven.m@gmail.com>
2018-08-12 14:47:56 -0400

    Hello World endpoint

---
+++ src/main/java/com/example/graphql/graphql/GraphQLEndpoint.java
@@ -0,0 +1,29 @@
+package com.example.graphql.graphql;
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.GraphQLError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GraphQLEndpoint {
+    @Autowired
+    GraphQL graphQL;
+
+    @PostMapping("/home")
+    public ResponseEntity<String> home(@RequestBody String query) {
+        System.out.println("query: " + query);
+        ExecutionResult execute = graphQL.execute(query);
+        List<GraphQLError> errors = execute.getErrors();
+        if (!errors.isEmpty())
+            return ResponseEntity.badRequest().body(errors.toString());
+
+        return ResponseEntity.ok(execute.getData());
+    }
+}
--- src/main/java/com/example/graphql/graphql/GraphqlApplication.java
+++ src/main/java/com/example/graphql/graphql/GraphqlApplication.java
@@ -1,12 +1,40 @@
 package com.example.graphql.graphql;
 
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
 
 @SpringBootApplication
 public class GraphqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraphqlApplication.class, args);
+	}
+
+	@Bean
+	public GraphQL graphQL() {
+		String schema = "type Query{hello: String}";
+
+		SchemaParser schemaParser = new SchemaParser();
+		TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+		RuntimeWiring runtimeWiring = newRuntimeWiring()
+				.type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+				.build();
+
+		SchemaGenerator schemaGenerator = new SchemaGenerator();
+		GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+		return GraphQL.newGraphQL(graphQLSchema).build();
 	}
 }
